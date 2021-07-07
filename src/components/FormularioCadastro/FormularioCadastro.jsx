60c1efbd5bfa56abb1ff6e83203eefa2d52c03ec
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-07-07 17:26:24 -0300

    create dadosusuario component

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import DadosUsuario from "./DadosUsuario";
 import DadosPessoais from "./DadosPessoais";
 
 function FormularioCadastro({ submit, validateCpf }) {
-  return <DadosPessoais />;
+  return (
+    <>
+      <DadosPessoais submit={submit} validateCpf={validateCpf} />
+      <DadosUsuario />
+    </>
+  );
 }
 
 export default FormularioCadastro;
