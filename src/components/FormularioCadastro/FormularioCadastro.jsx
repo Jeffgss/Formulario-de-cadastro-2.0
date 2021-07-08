bf49d9c8c91bc9a1ae948f29fd86b5bb7c4d8bb7
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-07-08 11:23:57 -0300

    refactor formulariocadastro component

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,29 +2,21 @@ import React, { useState } from "react";
 import DadosUsuario from "./DadosUsuario";
 import DadosPessoais from "./DadosPessoais";
 import DadosEntrega from "./DadosEntrega";
-import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ submit, validateCpf }) {
   const [currentStage, setCurrentStage] = useState(0);
+
+  const form = [
+    <DadosUsuario submit={next} />,
+    <DadosPessoais submit={next} validateCpf={validateCpf} />,
+    <DadosEntrega submit={submit} />,
+  ];
 
   function next() {
     setCurrentStage(currentStage + 1);
   }
 
-  function currentForm(stage) {
-    switch (stage) {
-      case 0:
-        return <DadosUsuario submit={next} />;
-      case 1:
-        return <DadosPessoais submit={next} validateCpf={validateCpf} />;
-      case 2:
-        return <DadosEntrega submit={submit} />;
-      default:
-        return <Typography>Página não encontrada</Typography>;
-    }
-  }
-
-  return <>{currentForm(currentStage)}</>;
+  return <>{form[currentStage]}</>;
 }
 
 export default FormularioCadastro;
