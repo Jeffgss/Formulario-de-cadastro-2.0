8d7db4d280877b00e46e83c79fd74bd369577f3a
Jeff Santos <jefferson.santos@maxmilhas.com.br>
2021-07-08 09:17:13 -0300

    create conditional rendering

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DadosUsuario from "./DadosUsuario";
 import DadosPessoais from "./DadosPessoais";
 import DadosEntrega from "./DadosEntrega";
+import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ submit, validateCpf }) {
-  return (
-    <>
-      <DadosPessoais submit={submit} validateCpf={validateCpf} />
-      <DadosUsuario />
-      <DadosEntrega />
-    </>
-  );
+  const [currentStage, setCurrentStage] = useState(0);
+
+  function currentForm(stage) {
+    switch (stage) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais submit={submit} validateCpf={validateCpf} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Página não encontrada</Typography>;
+    }
+  }
+
+  return <>{currentForm(currentStage)}</>;
 }
 
 export default FormularioCadastro;
