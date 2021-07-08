929d823843bd39bfa2e3c73273fd030bb47319ba
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-07-08 12:54:23 -0300

    collect data from all stages of the form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosUsuario from "./DadosUsuario";
 import DadosPessoais from "./DadosPessoais";
 import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ submit, validateCpf }) {
   const [currentStage, setCurrentStage] = useState(0);
+  const [collectedData, setCollectedData] = useState({});
+  useEffect(() => {
+    console.log(collectedData);
+  });
 
   const form = [
-    <DadosUsuario submit={next} />,
-    <DadosPessoais submit={next} validateCpf={validateCpf} />,
-    <DadosEntrega submit={submit} />,
+    <DadosUsuario submit={collectData} />,
+    <DadosPessoais submit={collectData} validateCpf={validateCpf} />,
+    <DadosEntrega submit={collectData} />,
   ];
+
+  function collectData(data) {
+    setCollectedData({ ...collectedData, ...data });
+    next();
+  }
 
   function next() {
     setCurrentStage(currentStage + 1);
