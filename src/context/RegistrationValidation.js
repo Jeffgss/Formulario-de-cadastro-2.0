805e0cb116c0ec39d7973226d91fd8aea0e45814
Jefferson Santos <jefferson.gsantos.dev@gmail.com>
2021-07-13 11:44:30 -0300

    create default validation value

--- src/context/RegistrationValidation.js
+++ src/context/RegistrationValidation.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const RegisterValidation = React.createContext();
+const RegisterValidation = React.createContext({
+  cpf: noValidation,
+  password: noValidation,
+  name: noValidation,
+});
+
+function noValidation(data) {
+  return { valid: true, text: "" };
+}
 
 export default RegisterValidation;
